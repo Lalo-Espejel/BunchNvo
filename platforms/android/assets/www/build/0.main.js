8c79d1c6e52266dbc3e29d56e7592facef15c50b
DESKTOP-IO4NKJ7\PC <jeduardo.espejel@gmail.com>
2017-11-13 11:28:59 -0600

    nuevos iconos para android

---
+++ platforms/android/assets/www/build/0.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([0],{
+
+/***/ 337:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GetSubDescripcionPageModule", function() { return GetSubDescripcionPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__get_sub_descripcion__ = __webpack_require__(130);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var GetSubDescripcionPageModule = (function () {
+    function GetSubDescripcionPageModule() {
+    }
+    return GetSubDescripcionPageModule;
+}());
+GetSubDescripcionPageModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__get_sub_descripcion__["a" /* GetSubDescripcionPage */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__get_sub_descripcion__["a" /* GetSubDescripcionPage */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__get_sub_descripcion__["a" /* GetSubDescripcionPage */]
+        ]
+    })
+], GetSubDescripcionPageModule);
+
+//# sourceMappingURL=get-sub-descripcion.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.main.js.map
