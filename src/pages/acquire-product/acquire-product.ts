82bafd9f36ae07ebcc992818e09e9f5d42772c77
DESKTOP-IO4NKJ7\PC <jeduardo.espejel@gmail.com>
2017-12-04 11:56:32 -0600

    nuevo build prueba

--- src/pages/acquire-product/acquire-product.ts
+++ src/pages/acquire-product/acquire-product.ts
@@ -637,7 +637,7 @@ export class AcquireProductPage {
     private userEdadList = [18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70];   
     private userDetalleList = [];
     private userSubDescriptionList = [];
-    private userSerialNumber = {name:'HEAH876542KLOP'}; //d
+    private userSerialNumber = {}; //d
     private userSerialNumberList = ['HEAH876542KLOP','HEAH87LOP','HEKLOP'];
     private userPlates = {name:'HEM987'}; //d
     private userPlatesList = ['HEM987','HEM9','HE87'];
