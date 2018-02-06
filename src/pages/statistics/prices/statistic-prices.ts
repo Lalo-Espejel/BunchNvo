bfc500bd6b46b5c4fa2c0645eb25822b3a716081
Martin Cisneros Capistrán <martin.cisneros@gmail.com>
2018-02-06 16:18:46 -0600

    ganancias y cotizaciones de statistics mejor acomodado

--- src/pages/statistics/prices/statistic-prices.ts
+++ src/pages/statistics/prices/statistic-prices.ts
@@ -69,108 +69,78 @@ export class StatisticPricesPage {
     public goBack = () => {
         this.navCtrl.pop();
     }
-    ionViewDidLoad() {
- 
-        this.barChart = new Chart(this.barCanvas.nativeElement, {
- 
-            type: 'bar',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-                }
-            }
- 
-        });
- 
-        this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
- 
-            type: 'doughnut',
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    hoverBackgroundColor: [
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56",
-                        "#FF6384",
-                        "#36A2EB",
-                        "#FFCE56"
-                    ]
-                }]
-            }
- 
-        });
+    ionViewDidLoad() {        
  
         this.lineChart = new Chart(this.lineCanvas.nativeElement, {
  
-            type: 'line',
+            type: 'line',            
             data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: ["1", "2", "3", "4", "5", "6", "7"],
                 datasets: [
                     {
-                        label: "My First dataset",
-                        fill: false,
-                        lineTension: 0.1,
-                        backgroundColor: "rgba(75,192,192,0.4)",
-                        borderColor: "rgba(75,192,192,1)",
+                        label: "", //My First dataset
+                        fill: "start",
+                        lineTension: 0.5,
+                        backgroundColor: "rgba(77,186,192,1)", //rgba(75,192,192,0.4)
+                        borderColor: "rgba(77,186,192,1)",
                         borderCapStyle: 'butt',
                         borderDash: [],
                         borderDashOffset: 0.0,
                         borderJoinStyle: 'miter',
-                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBorderColor: "rgba(77,186,192,1)",
                         pointBackgroundColor: "#fff",
-                        pointBorderWidth: 1,
+                        pointBorderWidth: 0, //1,
                         pointHoverRadius: 5,
-                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBackgroundColor: "rgba(77,186,192,1)",
                         pointHoverBorderColor: "rgba(220,220,220,1)",
                         pointHoverBorderWidth: 2,
-                        pointRadius: 1,
+                        pointRadius: 0, //1,
                         pointHitRadius: 10,
                         data: [65, 59, 80, 81, 56, 55, 40],
-                        spanGaps: false,
+                        spanGaps: false,                        
                     }
                 ]
+            },
+            options: {
+                legend: {
+                    labels: {
+                        usePointStyle: true,
+                        fontSize: 0,
+                    }
+                },
+                elements: { point: { radius: 0 } },
+                scales: {
+                    xAxes: [
+                        {
+                            ticks: {
+                                //display: false
+                                //beginAtZero: true,
+                                //min: 0,
+                            },
+                            gridLines: {
+                                display: false,
+                                drawBorder: false,
+                            }
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                display: false
+                            },
+                            gridLines: {
+                                display: false,
+                                drawBorder: false,
+                            }
+                        }
+                    ] //, {gridLines: {display: false}}
+                },                
+                tooltips: {
+                    displayColors: false,
+                }
             }
  
         });
- 
+
     }
 }
