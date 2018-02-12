cf4b92eb05b2f01facb7d9bd2d7371fbace93625
Martin Cisneros Capistrán <martin.cisneros@gmail.com>
2018-02-12 17:16:55 -0600

    la grafica ya esta basada en lo que contesta el api, esto en el area de ganancias

--- src/pages/statistics/earnings/statistic-earnings.ts
+++ src/pages/statistics/earnings/statistic-earnings.ts
@@ -2,8 +2,8 @@ import { Component, ViewChild  } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { StatisticProductsDetailsPage } from '../products-details/statistic-products-details';
 import { StatisticWeeksPage } from '../weeks/statistic-weeks';
-
 import { Chart } from 'chart.js';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     selector: 'statistic-earnings',
@@ -11,64 +11,22 @@ import { Chart } from 'chart.js';
 })
 export class StatisticEarningsPage {    
     @ViewChild('lineCanvas') lineCanvas;
- 
-    barChart: any;
-    doughnutChart: any;
+     
     lineChart: any;
     private weekInit:number = this.getWeekFirstDay();
     private weekEnd:number = this.getWeekLastDay();
-    private monthName:string = this.getMonthName();
-    private optionList:any =[];
-    private containerTexts = [
-        {
-            mainText: localStorage.getItem("language") == "en"?"MATERIAL DAMAGE: ":"DAÑOS MATERIALES: ",
-            subText: "5% V. COMERCIAL"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"TOTAL THEFT: ":"ROBO TOTAL: ",
-            subText: "10% V. COMERCIAL"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"RC PEOPLE: ":"RC PERSONAS: ",
-            subText: "3,000,000.00"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"RC: ":"RC: ",
-            subText: "800,000.00"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"LEGAL DEFENSE: ":"DEFENSA LEGAL: ",
-            subText: "AMPARADA"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"MEDICAL EXPENSES: ":"GASTOS MÉDICOS: ",
-            subText: "50,000.00"
-        },
-        {
-            mainText: localStorage.getItem("language") == "en"?"ROAD OF ASSISTANCE: ":"ASISTENCIA VIAL: ",
-            subText: "AMPARADA"
-        },
-    ];
+    private monthName:string = this.getMonthName();    
+    private totalEarnings:number = 0;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {}
 
-        let data = this.navParams.data;
-
-        this.optionList = [
-            {nameOfProduct:localStorage.getItem("language") == "en"?"Car insurance":"Seguro de Auto", subNameOfProduct:"Miguel Ivan Hernandez", companyLogo:"assets/icon/logo/logo-axa.png", companyName:"$12,500", companySubName:localStorage.getItem("language") == "en"?"Full payment":"Pago total", itemValue:"$2,500", itemSubValue: localStorage.getItem("language") == "en"?"Won":"Ganado", productDetails: {mainText: "GNP - Volkswagen Jetta", subText: localStorage.getItem("language") == "en"?"Posted on 15/04/2017":"Enviado el 15/04/2017"}},
-            {nameOfProduct:localStorage.getItem("language") == "en"?"Credit card":"Tarjeta de crédito", subNameOfProduct:"Miguel Ivan Hernandez", companyLogo:"assets/icon/logo/logo-axa.png", companyName:"$12,500", companySubName:localStorage.getItem("language") == "en"?"Full payment":"Pago total", itemValue:"$2,500", itemSubValue: localStorage.getItem("language") == "en"?"Won":"Ganado", productDetails: {mainText: "BBVA Bancomer - Premium Blac", subText: localStorage.getItem("language") == "en"?"Posted on 15/04/2017":"Enviado el 15/04/2017"}},
-            {nameOfProduct:localStorage.getItem("language") == "en"?"Credit card":"Tarjeta de crédito", subNameOfProduct:"Miguel Ivan Hernandez", companyLogo:"assets/icon/logo/logo-gnp.png", companyName:"$12,500", companySubName: localStorage.getItem("language") == "en"?"Full payment":"Pago total", itemValue:"$2,500", itemSubValue: localStorage.getItem("language") == "en"?"Won":"Ganado", productDetails: {mainText: "BBVA Bancomer - Premium Black", subText: localStorage.getItem("language") == "en"?"Posted on 15/04/2017":"Enviado el 15/04/2017"}}
-        ];
+    goToStatisticsProductsDetailsPage() {
+        this.navCtrl.push(StatisticProductsDetailsPage, {prevPage: 'chat'}, {animate: true});
     }
 
-    goToStatisticsProductsDetailsPage(){
-        this.navCtrl.push(StatisticProductsDetailsPage, {prevPage:"chat"}, {animate: true});
-    }
-    goToStatisticWeeksPage(){
-        this.navCtrl.push(StatisticWeeksPage, {prevPage:"chat"}, {animate: true});
-      }
-    public search = () => {
-    }
+    goToStatisticWeeksPage() {
+        this.navCtrl.push(StatisticWeeksPage, {prevPage: 'chat'}, {animate: true});
+    }    
 
     public goBack = () => {
         this.navCtrl.pop();
@@ -164,78 +122,152 @@ export class StatisticEarningsPage {
         return `${month}/${day}/${year}`;
     }
 
-    ionViewDidLoad() {        
- 
-        this.lineChart = new Chart(this.lineCanvas.nativeElement, {
- 
-            type: 'line',            
-            data: {
-                labels: ["1", "2", "3", "4", "5", "6", "7"],
-                datasets: [
-                    {
-                        label: "", //My First dataset
-                        fill: "start",
-                        lineTension: 0.5,
-                        backgroundColor: "rgba(77,186,192,1)", //rgba(75,192,192,0.4)
-                        borderColor: "rgba(77,186,192,1)",
-                        borderCapStyle: 'butt',
-                        borderDash: [],
-                        borderDashOffset: 0.0,
-                        borderJoinStyle: 'miter',
-                        pointBorderColor: "rgba(77,186,192,1)",
-                        pointBackgroundColor: "#fff",
-                        pointBorderWidth: 0, //1,
-                        pointHoverRadius: 5,
-                        pointHoverBackgroundColor: "rgba(77,186,192,1)",
-                        pointHoverBorderColor: "rgba(220,220,220,1)",
-                        pointHoverBorderWidth: 2,
-                        pointRadius: 0, //1,
-                        pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
-                        spanGaps: false,                        
-                    }
-                ]
-            },
-            options: {
-                legend: {
-                    labels: {
-                        usePointStyle: true,
-                        fontSize: 0,
-                    }
-                },
-                elements: { point: { radius: 0 } },
-                scales: {
-                    xAxes: [
-                        {
-                            ticks: {
-                                //display: false
-                                //beginAtZero: true,
-                                //min: 0,
-                            },
-                            gridLines: {
-                                display: false,
-                                drawBorder: false,
-                            }
-                        }
-                    ],
-                    yAxes: [
-                        {
-                            ticks: {
-                                display: false
-                            },
-                            gridLines: {
-                                display: false,
-                                drawBorder: false,
-                            }
-                        }
-                    ] //, {gridLines: {display: false}}
-                },                
-                tooltips: {
-                    displayColors: false,
-                }
-            }
- 
+    getDates(startDate, endDate) {
+        var dates = [],
+            currentDate = startDate,
+            addDays = function(days) {
+                var date = new Date(this.valueOf());
+                date.setDate(date.getDate() + days);
+                return date;
+            };
+        while (currentDate <= endDate) {
+            dates.push(currentDate);
+            currentDate = addDays.call(currentDate, 1);
+        }
+        return dates;
+    };            
+
+    ionViewDidLoad() {
+
+        var t = this;             
+        let actualWeek = this.getWeek(),        
+            fecIni = actualWeek.substr(7,10), // format mm/dd/yyyy
+            fecEnd = actualWeek.substr(actualWeek.length - 10), // format mm/dd/yyyy
+            fecIniArr = fecIni.split('/'),
+            fecEndArr = fecEnd.split('/'),
+            fecIniYear = +fecIniArr[2], fecIniMonth = +fecIniArr[0] -1, fecIniDay = +fecIniArr[1], fecEndYear = +fecEndArr[2], fecEndMonth = +fecEndArr[0] -1, fecEndDay = +fecEndArr[1],
+            dates = this.getDates(new Date(fecIniYear, fecIniMonth, fecIniDay), new Date(fecEndYear, fecEndMonth, fecEndDay)),
+            formatedDates = [];
+        
+        dates.forEach((date, index, arr) => {
+            formatedDates.push(t.formatDate(date));
+            arr[index] = date.getDate().toString();            
         });
- 
+
+        console.log({formatedDates});
+        var labels = dates;
+        console.log({labels});
+        
+        var lineChartData = [];
+        let encodedString;        
+
+        encodedString = btoa(`IdVend=1&fecIni=${formatedDates[0]}&fecFin=${formatedDates[0]}`)
+        t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+            lineChartData.push(+data.Ganancias.total);
+
+            encodedString = btoa(`IdVend=1&fecIni=${formatedDates[1]}&fecFin=${formatedDates[1]}`)
+            t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                lineChartData.push(+data.Ganancias.total);
+
+                encodedString = btoa(`IdVend=1&fecIni=${formatedDates[2]}&fecFin=${formatedDates[2]}`)
+                t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                    lineChartData.push(+data.Ganancias.total);
+
+                    encodedString = btoa(`IdVend=1&fecIni=${formatedDates[3]}&fecFin=${formatedDates[3]}`)
+                    t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                        lineChartData.push(+data.Ganancias.total);
+
+                        encodedString = btoa(`IdVend=1&fecIni=${formatedDates[4]}&fecFin=${formatedDates[4]}`)
+                        t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                            lineChartData.push(+data.Ganancias.total);
+
+                            encodedString = btoa(`IdVend=1&fecIni=${formatedDates[5]}&fecFin=${formatedDates[5]}`)
+                            t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                                lineChartData.push(+data.Ganancias.total);
+
+                                encodedString = btoa(`IdVend=1&fecIni=${formatedDates[6]}&fecFin=${formatedDates[6]}`)
+                                t.http.get(`http://services.bunch.guru/WebService.asmx/ConsultaEstadisticas?param=${encodedString}`).map(res => res.json()).subscribe(data => {
+                                    lineChartData.push(+data.Ganancias.total);
+                                                                        
+                                    //lineChartData = [65.2, 22, 11, 40, 23, 12, 44];
+                                    t.totalEarnings = lineChartData.reduce((prev, curr) => prev + curr);
+
+                                    t.lineChart = new Chart(t.lineCanvas.nativeElement, {
+                                        type: 'line',            
+                                        data: {
+                                            labels: labels,
+                                            datasets: [
+                                                {
+                                                    label: "", //My First dataset
+                                                    fill: "start",
+                                                    lineTension: 0.5,
+                                                    backgroundColor: "rgba(77,186,192,1)", //rgba(75,192,192,0.4)
+                                                    borderColor: "rgba(77,186,192,1)",
+                                                    borderCapStyle: 'butt',
+                                                    borderDash: [],
+                                                    borderDashOffset: 0.0,
+                                                    borderJoinStyle: 'miter',
+                                                    pointBorderColor: "rgba(77,186,192,1)",
+                                                    pointBackgroundColor: "#fff",
+                                                    pointBorderWidth: 0, //1,
+                                                    pointHoverRadius: 5,
+                                                    pointHoverBackgroundColor: "rgba(77,186,192,1)",
+                                                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                                                    pointHoverBorderWidth: 2,
+                                                    pointRadius: 0, //1,
+                                                    pointHitRadius: 10,
+                                                    data: lineChartData,
+                                                    spanGaps: false,                        
+                                                }
+                                            ]
+                                        },
+                                        options: {
+                                            legend: {
+                                                labels: {
+                                                    usePointStyle: true,
+                                                    fontSize: 0,
+                                                }
+                                            },
+                                            elements: { point: { radius: 0 } },
+                                            scales: {
+                                                xAxes: [
+                                                    {
+                                                        ticks: {
+                                                            //display: false
+                                                            //beginAtZero: true,
+                                                            //min: 0,
+                                                        },
+                                                        gridLines: {
+                                                            display: false,
+                                                            drawBorder: false,
+                                                        }
+                                                    }
+                                                ],
+                                                yAxes: [
+                                                    {
+                                                        ticks: {
+                                                            display: false,
+                                                            beginAtZero: true
+                                                        },
+                                                        gridLines: {
+                                                            display: false,
+                                                            drawBorder: false,
+                                                        }
+                                                    }
+                                                ] //, {gridLines: {display: false}}
+                                            },                
+                                            tooltips: {
+                                                displayColors: false,
+                                            }
+                                        }
+                            
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });     
     }
 }
