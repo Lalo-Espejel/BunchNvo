21bd6c5c92c5202c1c9718dad669de3baa6ed946
Martin Cisneros Capistrán <martin.cisneros@gmail.com>
2018-02-20 18:54:00 -0600

    elimine el error que tenia el chat, salia un alert

--- src/pages/texting/texting.ts
+++ src/pages/texting/texting.ts
@@ -10,69 +10,70 @@ import { RequestsProvider } from '../../providers/requests/requests';
   templateUrl: 'texting.html',
 })
 export class TextingPage {
-  buddy: any;
-  allmessages = [];
-  myfriends;
-  private prevPage: any;
-  public chats: Array<{id: string, img: string, name: string, lastMessage: string, updated: string }>
-
-
-  constructor(public zone: NgZone, public navCtrl: NavController, public events: Events, public requestservice: RequestsProvider, public navParams: NavParams, private _event: Events, public chatservice: ChatProvider) {
-    this.buddy = this.chatservice.buddy;
-    this.chats = [
-      { id: "0", img: "assets/icon/usuario-pendiente.png", name: "Miguel Hernández", lastMessage: "La anualidad 0 implica que no paga comisiones durante el año", updated: "14/06/16" },
-      { id: "1", img: "assets/icon/usuario-rechazado.png", name: "Maricela Palma", lastMessage: "Ok gracias, ya tengo seguro", updated: "14/06/16" },
-      { id: "2", img: "assets/icon/usuario-espera.png", name: "Daniel Benitez", lastMessage: "Hola Daniel, te recomiendo que no compres valores de mercado.", updated: "14/06/16" },
-      { id: "3", img: "assets/icon/usuario-pendiente.png", name: "Eduardo Luna", lastMessage: "Prueba con el producto que te recomendé el otro día.", updated: "14/06/16" }
-    ]
-    this.prevPage = this.navParams.get("prevPage");
-    if(this.prevPage == "chat"){
-      this._event.publish('chat:products');
-    }
-    this.updateFriends();
-    this.events.subscribe('newmessage', () => {
-      this.allmessages = [];
-      this.zone.run(() => {
-        this.allmessages = this.chatservice.buddymessages;
-        console.log("los mensajes"+this.allmessages);
-      })
-    })  
     
-  }
+    buddy: any;
+    allmessages = [];
+    myfriends;
+    private prevPage: any;
+    public chats: Array<{id: string, img: string, name: string, lastMessage: string, updated: string }>
 
- public seeChat = (chat) => {
-   this.navCtrl.push(ChatPage, chat, { animate: true });
- }
 
- updateFriends() {
-  var cont=0;
-  this.requestservice.getmyfriends();
-  this.myfriends = [];
-  this.events.subscribe('friends', () => {
-    this.myfriends = [];
-    this.myfriends = this.requestservice.myfriends; 
-    for (let key of this.myfriends){
-      //console.log("encontramos al amigo"+JSON.stringify(this.myfriends[cont].displayName));  
-      this.chats.push({id: "2", img: "assets/icon/usuario-espera.png", name: this.myfriends[cont].displayName, lastMessage: "Hola Nuevo", updated: "14/06/16"});
-      cont++;
-      console.log("esto es palaba"+JSON.stringify(this.myfriends));
-      this.chatservice.initializebuddy(this.myfriends);
-      this.chatservice.getbuddymessages();
+    constructor(public zone: NgZone, public navCtrl: NavController, public events: Events, public requestservice: RequestsProvider, public navParams: NavParams, private _event: Events, public chatservice: ChatProvider) {        
+        this.buddy = this.chatservice.buddy;
+        this.chats = [
+            { id: "0", img: "assets/icon/usuario-pendiente.png", name: "Miguel Hernández", lastMessage: "La anualidad 0 implica que no paga comisiones durante el año", updated: "14/06/16" },
+            { id: "1", img: "assets/icon/usuario-rechazado.png", name: "Maricela Palma", lastMessage: "Ok gracias, ya tengo seguro", updated: "14/06/16" },
+            { id: "2", img: "assets/icon/usuario-espera.png", name: "Daniel Benitez", lastMessage: "Hola Daniel, te recomiendo que no compres valores de mercado.", updated: "14/06/16" },
+            { id: "3", img: "assets/icon/usuario-pendiente.png", name: "Eduardo Luna", lastMessage: "Prueba con el producto que te recomendé el otro día.", updated: "14/06/16" }
+        ];        
+        this.prevPage = this.navParams.get("prevPage");
+        if (this.prevPage == "chat") {
+            this._event.publish('chat:products');
+        }        
+        this.updateFriends();        
+        this.events.subscribe('newmessage', () => {            
+            this.allmessages = [];
+            this.zone.run(() => {                
+                this.allmessages = this.chatservice.buddymessages;
+                console.log("los mensajes", this.allmessages);
+            });
+        });
     }
-    
-  })
-}
 
-  addbuddy() {
-    this.navCtrl.push('BuddiesPage');
-  }
+    public seeChat = (chat) => {
+        this.navCtrl.push(ChatPage, chat, { animate: true });
+    }
 
-  public buddychat = (chat) => {
-    console.log("para buddy chat"+JSON.stringify(chat));
-    this.chatservice.initializebuddy(chat);
-    this.navCtrl.push(ChatPage, chat, { animate: true });
-  }
+    updateFriends() {
+        var cont = 0;
+        this.requestservice.getmyfriends();
+        this.myfriends = [];
+        this.events.subscribe('friends', () => {
+            this.myfriends = [];
+            this.myfriends = this.requestservice.myfriends;
+            console.log('this.myfriends', this.myfriends);
+            
+            for (let key of this.myfriends){                
+                this.chats.push({
+                    id: "2", 
+                    img: "assets/icon/usuario-espera.png", 
+                    name: this.myfriends[cont].displayName, 
+                    lastMessage: "Hola Nuevo", 
+                    updated: "14/06/16"
+                });
+                cont++;                                
+                this.chatservice.initializebuddy(this.myfriends);
+                this.chatservice.getbuddymessages();
+            }
+        });
+    }
 
-  
+    addbuddy() {
+        this.navCtrl.push('BuddiesPage');
+    }
 
+    public buddychat = (chat) => {        
+        this.chatservice.initializebuddy(chat);
+        this.navCtrl.push(ChatPage, chat, { animate: true });
+    }
 }
--- src/providers/chat/chat.ts
+++ src/providers/chat/chat.ts
@@ -4,8 +4,6 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import firebase from 'firebase';
 
-
-
 /*
   Generated class for the ChatProvider provider.
 
@@ -14,48 +12,51 @@ import firebase from 'firebase';
 */
 @Injectable()
 export class ChatProvider {
-  firebuddychats = firebase.database().ref('/buddychats');
-  buddy: any;
-  buddymessages = [];
-  constructor(public http: Http, public events: Events) {
-    console.log('Hello ChatProvider Provider');
-  }
+    firebuddychats = firebase.database().ref('/buddychats');
+    buddy: any;
+    buddymessages = [];
+    constructor(public http: Http, public events: Events) {}
 
-  initializebuddy(buddy) {
-    this.buddy = buddy;
-  }
-
-  getbuddymessages() {
-    
-    let temp;
-    this.firebuddychats.child(firebase.auth().currentUser.uid).child(this.buddy.uid).on('value', (snapshot) => {
-      this.buddymessages = [];
-      temp = snapshot.val();
-      for (var tempkey in temp) {
-        this.buddymessages.push(temp[tempkey]);
-      }
-      this.events.publish('newmessage');
-    })
-  }
-
-  addnewmessage(msg) {
-    if (this.buddy) {
-      var promise = new Promise((resolve, reject) => {
-        this.firebuddychats.child(firebase.auth().currentUser.uid).child(this.buddy.uid).push({
-          sentby: firebase.auth().currentUser.uid,
-          message: msg,
-          timestamp: firebase.database.ServerValue.TIMESTAMP
-        }).then(() => {
-          this.firebuddychats.child(this.buddy.uid).child(firebase.auth().currentUser.uid).push({
-            sentby: firebase.auth().currentUser.uid,
-            message: msg,
-            timestamp: firebase.database.ServerValue.TIMESTAMP
-          }).then(() => {
-            resolve(true);
-            })
-        })
-      })
-      return promise;
+    initializebuddy(buddy) {
+        console.log('initializebuddy buddy', buddy);
+        this.buddy = buddy;
     }
-  }  
+
+    getbuddymessages() {
+        console.log('getbuddymessages');
+        console.log('this.buddy', this.buddy);
+        let temp;
+        let buddyUid = this.buddy.uid || this.buddy[0].uid; //antes era: this.buddy.uid
+        if (buddyUid != undefined) {
+            this.firebuddychats.child(firebase.auth().currentUser.uid).child(buddyUid).on('value', (snapshot) => {
+                this.buddymessages = [];
+                temp = snapshot.val();
+                for (var tempkey in temp) {
+                    this.buddymessages.push(temp[tempkey]);
+                }
+                this.events.publish('newmessage');
+            });
+        }        
+    }
+
+    addnewmessage(msg) {
+        if (this.buddy) {
+            var promise = new Promise((resolve, reject) => {
+                this.firebuddychats.child(firebase.auth().currentUser.uid).child(this.buddy.uid).push({
+                    sentby: firebase.auth().currentUser.uid,
+                    message: msg,
+                    timestamp: firebase.database.ServerValue.TIMESTAMP
+                }).then(() => {
+                    this.firebuddychats.child(this.buddy.uid).child(firebase.auth().currentUser.uid).push({
+                        sentby: firebase.auth().currentUser.uid,
+                        message: msg,
+                        timestamp: firebase.database.ServerValue.TIMESTAMP
+                    }).then(() => {
+                        resolve(true);
+                    })
+                })
+            })
+            return promise;
+        }
+    }  
 }
